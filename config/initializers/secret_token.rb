f649b1768979ab982a2ec437232c9db5442e69da
David Berko <davidberko19@gmail.com>
2012-07-13 11:50:02 -0500

    Added successful authentication for a user using bcrypt-ruby

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Authentication::Application.config.secret_token = 'a4b01d51dc49afd63050bf80fefd13fc855decaf0d85fae81b2806c8db257976a3597abc48e07a5bc6b145fbe0a64e1cf46b6bab6ac4cd43904382526d29bbdb'
